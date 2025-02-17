d325375ff46964098e1d4c99ca619cc8c74b5f89
uhamhz <ammarhafizh2404@gmail.com>
2025-02-17 09:36:13 +0700

    update : try route output

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/welcome', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/data', function () {
+    return 'Muhammad Ammar Hafizh - 2341720074';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($post, $comment) {
+    return 'Post ke-' . $post . ' Komentar ke-' . $comment;
+});
+
+Route::get('/article/{id}', function ($id) {
+    return 'Artikel ke-' . $id;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
